0a4156a10444366944a2893f6a90a8225fdf6227
Kyle Scudder <kyle@kylescudder.co.uk>
2026-06-12 16:59:56 +0100

    Add balm cask (0.1.0)

---
+++ Casks/balm.rb
@@ -0,0 +1,26 @@
+cask "balm" do
+  version "0.1.0"
+  sha256 "9d7b0d7664fd85b779d058278d477f0abe994b19daeeec939ad90740c2e052cc"
+
+  url "https://github.com/kylescudder/balm/releases/download/v#{version}/Balm-#{version}-macos.zip"
+  name "Balm"
+  desc "Native, keyboard-driven SwiftUI client for Jira Cloud"
+  homepage "https://github.com/kylescudder/balm"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  depends_on macos: :sequoia
+
+  app "Balm.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args: ["-dr", "com.apple.quarantine", "#{appdir}/Balm.app"],
+                   sudo: false
+  end
+
+  zap trash: "~/Library/Preferences/app.balm.plist"
+end
