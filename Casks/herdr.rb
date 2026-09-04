c2f181aa165cc2790d028ca8bd919f39d1f6a8fc
Kyle Scudder <74150974+kylescudder@users.noreply.github.com>
2026-09-04 07:46:30 +0100

    herdr 0.8.10

--- Casks/herdr.rb
+++ Casks/herdr.rb
@@ -1,9 +1,9 @@
 cask "herdr" do
   arch arm: "aarch64", intel: "x86_64"
 
-  version "0.8.9"
-  sha256 arm:   "02258793e4c86ae0dce7d2d6a9e78f786024c9c97b036da37dda6c27e1835ed7",
-         intel: "1e06e5c6662c0979b9367576d6a7112ce33ceb0e4503bac17debeea6d60245df"
+  version "0.8.10"
+  sha256 arm:   "152b52f6be211974501e10db094dfa353b865cd3c60c053be7b96be3e02cad47",
+         intel: "c599d1249e038ee02609e962b80f7d1b0db352544db57bdacab94212e3a315a2"
 
   url "https://github.com/kylescudder/herdr/releases/download/v#{version}/herdr-macos-#{arch}"
   name "herdr"
